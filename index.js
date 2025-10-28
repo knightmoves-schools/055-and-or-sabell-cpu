d2efa285fdfd6efa167a21447b2a1a1402cfe9b1
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-28 18:10:04 +0000

    and-or

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function taxable(state, taxExempt) {
+  if (state === "IA" && taxExempt === false) {
+    return "is taxable";
+  }
+}
 
+function isVehicle(hasWheels, canFly, canSwim) {
+  if (hasWheels || canFly || canSwim) {
+    return "is vehicle";
+  }
+}
